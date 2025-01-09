93033a5da7d68705413f43e03e7fef398d719194
Henry Bergström <henrybergstrom@protonmail.com>
2025-01-09 19:20:20 +0700

    add url form

--- script.js
+++ script.js
@@ -4,11 +4,48 @@
 const endpoint = "https://swapi.py4e.com/api/people/1"; 
 fetch(endpoint)
     .then(resp => resp.json()) // Omvandla svaret till ett objekt från json
-    .then(data => console.log(data)) // TODO: Visa datan på sidan
+    .then(data => renderData(data)) // TODO: Visa datan på sidan
     .catch(err => console.log("Something went wrong", err));
 
+const formEl = document.getElementById("form");
+formEl.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const urlEl = document.getElementById("url");
+    console.log(urlEl.value);
+
+    // Kalla på API-endpunkten som användare har skrivit in
+    const endpointFromUser = urlEl.value;
+    fetch(endpointFromUser)
+        .then(resp => resp.json()) // Omvandla svaret till ett objekt från json
+        .then(data => renderData(data)) // TODO: Visa datan på sidan
+        .catch(err => console.log("Something went wrong", err));
+})
 
 function renderData(data) {
     const resultDivEl = document.getElementById("result");
+    console.log(data);
     // Ej Implementerad än
+
+    // const obj = {a: 1, b: 2, c: 3};
+    // for (const prop in obj) {
+    //     const num = obj[prop];
+    // }
+
+    // Undersök vilka properties som finns i data
+    // genom att loopa med for .. in
+    resultDivEl.innerHTML = "<ul>";
+    for (const prop in data) {
+        console.log(prop, data[prop]);
+
+        resultDivEl.innerHTML += `<li>${prop}: ${data[prop]}</li>`;
+    }
+    resultDivEl.innerHTML += "</ul>";
+
+
+
+    // resultDivEl.innerHTML = `<div>
+    //                             <h2>${data.name}</h2>
+    //                             <p>${data.hair_color}</p>
+    //                             <p>${data.height}</p>
+    //                         </div>`;
 }

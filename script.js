205f052eb4f4b61f0387423bc403e7a8b23ce41d
Henry Bergström <henrybergstrom@protonmail.com>
2025-01-14 16:16:23 +0700

    pokemon gallery

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// API-endpoint
-// En URL som pekar på en resurs
-// I detta fallet en 'people' med id 1 från star wars
-const endpoint = "https://swapi.py4e.com/api/people/1"; 
-fetch(endpoint)
-    .then(resp => resp.json()) // Omvandla svaret till ett objekt från json
-    .then(data => renderData(data)) // TODO: Visa datan på sidan
-    .catch(err => console.log("Something went wrong", err));
+const resultDivEl = document.getElementById("result");
 
 const formEl = document.getElementById("form");
 formEl.addEventListener("submit", (evt) => {
     evt.preventDefault();
+    resultDivEl.innerHTML = "";
     const urlEl = document.getElementById("url");
     console.log(urlEl.value);
 
@@ -17,12 +11,11 @@ formEl.addEventListener("submit", (evt) => {
     const endpointFromUser = urlEl.value;
     fetch(endpointFromUser)
         .then(resp => resp.json()) // Omvandla svaret till ett objekt från json
-        .then(data => renderData(data)) // TODO: Visa datan på sidan
+        .then(data => renderData(data)) // DONE: Visa datan på sidan
         .catch(err => console.log("Something went wrong", err));
 })
 
 function renderData(data) {
-    const resultDivEl = document.getElementById("result");
     console.log(data);
     // Ej Implementerad än
 
@@ -33,19 +26,13 @@ function renderData(data) {
 
     // Undersök vilka properties som finns i data
     // genom att loopa med for .. in
-    resultDivEl.innerHTML = "<ul>";
+    resultDivEl.innerHTML += "<ul>";
     for (const prop in data) {
         console.log(prop, data[prop]);
 
-        resultDivEl.innerHTML += `<li>${prop}: ${data[prop]}</li>`;
+        
+        resultDivEl.innerHTML += `<li><span class="prop">${prop}:</span> ${data[prop]}</li>`;
+        
     }
     resultDivEl.innerHTML += "</ul>";
-
-
-
-    // resultDivEl.innerHTML = `<div>
-    //                             <h2>${data.name}</h2>
-    //                             <p>${data.hair_color}</p>
-    //                             <p>${data.height}</p>
-    //                         </div>`;
 }
